0b825a07995f99151de040e4e6fc4bff07a77598
Giovanni Lammoglia <giovannifvl@gmail.com>
2017-08-26 09:57:22 -1000

    added login container

---
+++ src/containers/login.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: '',
+      password: ''
+    }
+
+
+  }
+
+  render() {
+    return (
+      <div>
+        <form>
+          <div className="form-group">
+            <label htmlFor="exampleInputEmail1">Email address</label>
+            <input type="email" className="form-control" id="exampleInputEmail1" placeholder="Email"
+                   onChange={(event)=>{this.setState({username: event.target.value})}}
+                   value={this.state.username}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="exampleInputPassword1">Password</label>
+            <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"
+                   onChange={event=>{this.setState({password: event.target.value})}}
+                   value={this.state.password}
+            />
+          </div>
+          <button type="submit" className="btn btn-default">Submit</button>
+        </form>
+      </div>
+    )
+  }
+
+
+}
+
+export default Login;
+
