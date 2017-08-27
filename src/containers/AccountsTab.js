178a9d7b572800735263e7f69598203bf829598a
Giovanni Lammoglia <giovannifvl@gmail.com>
2017-08-26 14:12:45 -1000

    made accounts table and added to route

---
+++ src/containers/AccountsTab.js
@@ -0,0 +1,140 @@
+import React, {Component} from 'react';
+import {Route, IndexRoute} from 'react-router-dom';
+import firebase from '../firebase';
+
+class NewAccountInput extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      accountName: '',
+      accountNumber: '',
+      clientName: '',
+      matterTitle: '',
+      items:[]
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const itemsRef = firebase.database().ref('items');
+    const item = {
+      accountName: this.state.accountName,
+      accountNumber: this.state.accountNumber,
+      clientName: this.state.clientName,
+      matterTitle: this.state.matterTitle
+    };
+    itemsRef.push(item);
+    this.setState({
+      accountName: '',
+      accountNumber: '',
+      clientName: '',
+      matterTitle: ''
+    })
+
+  }
+
+  componentDidMount() {
+    const itemsRef = firebase.database().ref('items');
+    itemsRef.on('value', (snapshot)=> {
+      let items = snapshot.val();
+      let newState = [];
+      for (let item in items) {
+        newState.push({
+          id: item,
+          accountName: items[item].accountName,
+          accountNumber: items[item].accountNumber,
+          clientName: items[item].clientName,
+          matterTitle: items[item].matterTitle
+
+        });
+      }
+      this.setState({
+        items: newState
+      })
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <form className="form-horizontal" onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="inputAccountName" className="col-sm-2 control-label">Account Name</label>
+            <div className="col-sm-10">
+              <input type="text" className="form-control" id="inputAccountName" name="accountName"
+                     onChange={this.handleChange} value={this.state.accountName} placeholder=""/>
+            </div>
+          </div>
+          <div className="form-group">
+            <label htmlFor="inputAccountNumber" className="col-sm-2 control-label">Account Number</label>
+            <div className="col-sm-10">
+              <input type="text" className="form-control" id="inputAccountNumber" name="accountNumber"
+                     onChange={this.handleChange} value={this.state.accountNumber} placeholder=""/>
+            </div>
+          </div>
+          <div className="form-group">
+            <label htmlFor="inputClientName" className="col-sm-2 control-label">Client Name</label>
+            <div className="col-sm-10">
+              <input type="text" className="form-control" id="inputClientName" name="clientName"
+                     onChange={this.handleChange} value={this.state.clientName} placeholder=""/>
+            </div>
+          </div>
+          <div className="form-group">
+            <label htmlFor="inputMatterTitle" className="col-sm-2 control-label">Matter Title</label>
+            <div className="col-sm-10">
+              <input type="text" className="form-control" id="inputMatterTitle" name="matterTitle"
+                     onChange={this.handleChange} value={this.state.matterTitle} placeholder=""/>
+            </div>
+          </div>
+          <div className="form-group">
+            <div className="col-sm-offset-2 col-sm-10">
+            </div>
+          </div>
+          <div className="form-group">
+            <div className="col-sm-offset-2 col-sm-10">
+              <button type="submit" className="btn btn-default">Submit</button>
+            </div>
+          </div>
+        </form>
+        <div className="wrapper">
+          <table className="table table-bordered">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Account Name</th>
+                <th>Account Number</th>
+                <th>Client Name</th>
+                <th>Matter Title</th>
+              </tr>
+            </thead>
+            <tbody>
+            {this.state.items.map((item)=> {
+              return (
+                <tr key={item.id}>
+                  <th>#</th>
+                  <td>{item.accountName}</td>
+                  <td>{item.accountNumber}</td>
+                  <td>{item.clientName}</td>
+                  <td>{item.matterTitle}</td>
+                </tr>
+              )
+            })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default NewAccountInput;
--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,13 @@ import {Route} from 'react-router-dom';
 import App from './components/app';
 import Login from './containers/login';
 import TimeSheet from './containers/timesheet';
+import AccountsTab from './containers/AccountsTab';
 
 export default () => (
   <div>
     <Route path="/" component={App} />
     <Route path="/login" component={Login} />
     <Route path="/timesheet" component={TimeSheet} />
+    <Route path="/accounts" component={AccountsTab} />
   </div>
 )
